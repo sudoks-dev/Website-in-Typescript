d4ec6eb7037f750fd160e10974a11644434674da
Sudo <dev.sudoks@gmail.com>
2024-05-13 14:02:43 +0530

    added submit button in contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -76,16 +76,10 @@ export const Contact = () => {
                     <Col size={12} sm={6} className="px-1">
                       <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
                     </Col>
-                    {/* <Col size={12} className="px-1">
-                      <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
+                    <Col size={12} className="px-1">
+                      <input type="submit" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)} />
                       <button type="submit"><span>{buttonText}</span></button>
                     </Col>
-                    {
-                      status.message &&
-                      <Col>
-                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
-                      </Col>
-                    } */}
                   </Row>
                 </form>
               </div>}
